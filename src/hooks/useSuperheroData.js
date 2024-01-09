8d584035e4ced7ee292b88cc0b57640be2cc826b
vipulpathak113 <vipulpathak113@gmail.com>
2024-01-09 15:01:40 +0530

    initial data

--- src/hooks/useSuperheroData.js
+++ src/hooks/useSuperheroData.js
@@ -1,9 +1,27 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 export const useSuperheroData = (heroId) => {
-  return useQuery(["super-hero", heroId], ({ queryKey }) => {
-    const id = queryKey[1];
-    return axios.get(`http://localhost:4000/superheroes/${id}`);
-  });
+  const queryClient = useQueryClient();
+  return useQuery(
+    ["super-hero", heroId],
+    ({ queryKey }) => {
+      const id = queryKey[1];
+      return axios.get(`http://localhost:4000/superheroes/${id}`);
+    },
+    {
+      initialData: () => {
+        const hero = queryClient
+          .getQueryData("super-heroes")
+          ?.data?.find((hero) => hero.id === heroId);
+        if (hero) {
+          return {
+            data: hero,
+          };
+        } else {
+          return undefined;
+        }
+      },
+    }
+  );
 };
